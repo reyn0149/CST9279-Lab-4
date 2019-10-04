16dfc947e1737cf561e2a5b3d55d7fddb448902a
reyn0149 <55161634+reyn0149@users.noreply.github.com>
2019-10-04 16:09:00 -0400

    Add files via upload

--- calculateDistanceBetweenPoints.py
+++ calculateDistanceBetweenPoints.py
@@ -1,4 +1,4 @@
 # A program to test importing the function calculateDistanceBetweenPoints and then tests it with four integers for point cordinates.
 from reynLibrary import calculateDistanceBetweenPoints
 
-calculateDistanceBetweenPoints(5,10,8,16)
+print(calculateDistanceBetweenPoints(5,10,8,16))
--- calculateMpg.py
+++ calculateMpg.py
@@ -1,4 +1,4 @@
 # A program to test importing the calculateMpg function and then tests it with two integers for miles and then gallons.
 from reynLibrary import calculateMpg
 
-calculateMpg(450,10)
+print (calculateMpg(450,10))
